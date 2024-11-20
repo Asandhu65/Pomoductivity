45ca8c1e38336e4d728afe25c8631600c3c56386
Aman Sandhu <asandhu5656@gmail.com>
2024-11-20 15:02:37 -0500

    First commit.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./output.css";
+import "./index.css";
+import { Button } from "./components/ui/button.jsx";
+
+function App() {
+  return (
+    <div>
+      <Button className="DEFAULT bg-opacity-40 shadow-lg p-2">
+        <div className="[text-shadow:_2.5px_2px_3px_rgb(0_0_0_/_100%)]">
+          To-Do List
+        </div>
+      </Button>
+    </div>
+  );
+}
+
+export default App;
