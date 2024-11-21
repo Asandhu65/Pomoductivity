1cdcaaec643a1b57f9017613a102e321f8231913
Aman Sandhu <asandhu5656@gmail.com>
2024-11-21 17:21:50 -0500

    Added timer component library.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,40 @@ import "./output.css";
 import "./index.css";
 import { Button } from "./components/ui/button.jsx";
 
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+const minuteSeconds = 60;
+// const hourSeconds = 3600;
+// const daySeconds = 86400;
+
+const timerProps = {
+  isPlaying: true,
+  size: 500,
+  strokeWidth: 12,
+};
+
+const renderTimer = (dimension, time) => {
+  return (
+    <div>
+      <div>{time}</div>
+      <div>{dimension}</div>
+    </div>
+  );
+};
+
+const getTimeSeconds = time => (minuteSeconds - time) | 0;
+// const getTimeMinutes = time => ((time % hourSeconds) / minuteSeconds) | 0;
+// const getTimeHours = time => ((time % daySeconds) / hourSeconds) | 0;
+// const getTimeDays = time => (time / daySeconds) | 0;
+
 function App() {
+  const startTime = Date.now() / 1000; // use UNIX timestamp in seconds
+  const endTime = startTime + 243248; // use UNIX timestamp in seconds
+
+  const remainingTime = endTime - startTime;
+  // const days = Math.ceil(remainingTime / daySeconds);
+  // const daysDuration = days * daySeconds;
+
   return (
     <div>
       <Button className="DEFAULT bg-opacity-40 shadow-lg p-2">
@@ -10,6 +43,21 @@ function App() {
           To-Do List
         </div>
       </Button>
+      <CountdownCircleTimer
+        {...timerProps}
+        colors="white"
+        duration={minuteSeconds}
+        initialRemainingTime={remainingTime % minuteSeconds}
+        onComplete={totalElapsedTime => ({
+          shouldRepeat: remainingTime - totalElapsedTime > 0,
+        })}
+      >
+        {({ elapsedTime, color }) => (
+          <span style={{ color }}>
+            {renderTimer("seconds", getTimeSeconds(elapsedTime))}
+          </span>
+        )}
+      </CountdownCircleTimer>
     </div>
   );
 }
